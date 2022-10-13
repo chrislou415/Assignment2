ebc8d55ce58f119d9c8fa3be27d0ee3d432c2720
ChrisL <113984397+chrislou415@users.noreply.github.com>
2022-10-13 12:18:20 +1100

    Add files via upload

---
+++ js/vega_lite.js
@@ -0,0 +1,12 @@
+var vg_1 = "/FIT3179 A2/js/graph1.json"
+var vg_2 = "/FIT3179 A2/js/graph2.json"
+
+vegaEmbed('#first_chart', vg_2).then(function (result) {
+
+}).catch(console.error);
+
+vegaEmbed('#second_chart', vg_1).then(function (result) {
+
+}).catch(console.error);
+
+
