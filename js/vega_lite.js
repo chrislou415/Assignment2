b30650bb0852b08adc54eaf6a96027b0212fb6f3
ChrisL <113984397+chrislou415@users.noreply.github.com>
2022-10-13 13:17:49 +1100

    Update vega_lite.js

--- js/vega_lite.js
+++ js/vega_lite.js
@@ -1,11 +1,6 @@
-var vg_1 = "/FIT3179 A2/js/graph1.json"
-var vg_2 = "/FIT3179 A2/js/graph2.json"
+var vg_1 = "js/graph1.json"
 
 vegaEmbed('#first_chart', vg_1).then(function (result) {
-
-}).catch(console.error);
-
-vegaEmbed('#second_chart', vg_2).then(function (result) {
 
 }).catch(console.error);
 
